7865109a3ddb586cf74fc9398b288c7a1531e6b0
MitanshiKshatriya <mitanshikshatriya@gmail.com>
2021-10-06 03:46:21 +0530

    protected routes

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,9 +2,15 @@ import React from 'react'
 import {BrowserRouter, Switch, Route }
 from "react-router-dom" 
 
+import PrivateRoutes from './auth/helper/PrivateRoutes'
+import AdminRoutes from './auth/helper/AdminRoutes'
+
 import Home from "./core/Home"
 import Signin from './user/Signin'
 import Signup from "./user/Signup"
+import Profile from "./user/Profile"
+import UserDashBoard from "./user/UserDashBoard"
+import AdminDashBoard from "./user/AdminDashBoard"
 
 const Routes = () => {
     return (
@@ -13,6 +19,9 @@ const Routes = () => {
                <Route path="/" exact component={Home}/>
                <Route path="/signup" exact component={Signup}/>
                <Route path="/signin" exact component={Signin}/>
+               <PrivateRoutes path="/profile" exact component={Profile}/>
+               <PrivateRoutes path="/user/dashboard" exact component={UserDashBoard}/>
+               <AdminRoutes path="/admin/dashboard" exact component={AdminDashBoard}/>
            </Switch>
        </BrowserRouter>
     )
