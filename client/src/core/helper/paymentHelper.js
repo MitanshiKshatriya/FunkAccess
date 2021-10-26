e2ceeb2c7f38d931bb0c6ca405a904806a3337d6
MitanshiKshatriya <mitanshikshatriya@gmail.com>
2021-10-26 21:32:20 +0530

    convert to async

--- client/src/core/helper/paymentHelper.js
+++ client/src/core/helper/paymentHelper.js
@@ -1,34 +1,37 @@
 import { API } from "../../backend";
 
-export const getmeToken = (userId, token) => {
-    return fetch(`${API}/payment/gettoken/${userId}`, {
-    method: "GET",
-    headers: {
-        "Content-type": "application/json",
-        Accept: "application/json",
-        Authorization: `Bearer ${token}`
-
+export const getmeToken = async (userId, token, user) => {
+    console.log(userId)
+    try {
+        const response = await fetch(`${API}/payment/gettoken/${userId}`, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`,
+            }
+        }
+        );
+        return await response.json();
+    } catch (err) {
+        return console.log(err);
     }
-}
-    )
-    .then(response=> response.json())
-    .catch(err=> console.log(err))
 } 
 
-export const proccessPayment = (userId, token, paymentInfo) => {
-    return fetch(`${API}/payment/braintree/${userId}`, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json",
-            Accept: "application/json",
-            Authorization: `Bearer ${token}`
-    
-        },
-        body: JSON.stringify(paymentInfo)
+export const proccessPayment = async (userId, token, paymentInfo) => {
+    try {
+        const response = await fetch(`${API}/payment/braintree/${userId}`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                Accept: "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify(paymentInfo)
+        }
+        );
+        return await response.json();
+    } catch (err) {
+        return console.log(err);
     }
-        )
-    .then( response => {
-        return response.json()
-    } )
-    .catch(err=> console.log(err))
 }
